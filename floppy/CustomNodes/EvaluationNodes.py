232e522b3dbec7f3669393a2da841751b677cb94
fukatani <nannyakannya@gmail.com>
2017-02-11 10:18:55 +0900

    Fix accuracy

--- floppy/CustomNodes/EvaluationNodes.py
+++ floppy/CustomNodes/EvaluationNodes.py
@@ -9,7 +9,8 @@ import numpy
 
 class Accuracy(Node):
     Input('in_array', chainer.Variable)
+    Input('ground_truth', chainer.Variable)
     Output('accuracy', numpy.ndarray)
 
     def run(self):
-        self._accuracy = functions.accuracy(self._in_array)
+        self._accuracy = functions.accuracy(self._in_array, self._ground_truth)
