1c3dfd23cfd553d4d8026b383dfd316f79e67e0a
fukatani <nannyakannya@gmail.com>
2017-02-12 21:56:18 +0900

    Refactoring

--- floppy/CustomNodes/LossNodes.py
+++ floppy/CustomNodes/LossNodes.py
@@ -7,7 +7,6 @@ from chainer import functions
 class SoftmaxCrossEntropy(Loss):
     Input('in_array', chainer.Variable)
     Input('ground_truth', chainer.Variable)
-    Output('out_array', chainer.Variable)
 
     def run(self):
         self._out_array = functions.softmax_cross_entropy(self._in_array,
