689c4e42e234b88dfd2fa5e3cbc5a55f48f171e6
fukatani <nannyakannya@gmail.com>
2017-02-18 21:01:21 +0900

    Add compiler test

--- floppy/test/test_compiler.py
+++ floppy/test/test_compiler.py
@@ -1,5 +1,24 @@
 from floppy.graph import Graph
+from floppy import node_lib
+from floppy import train_configuration
+from floppy.painter import MainWindow, Painter2D
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtCore import QSettings
+import sys
+import filecmp
+
 
 if __name__ == '__main__':
-    graph = Graph().loadDict('../../examples/mnist.ppy')
-    graph.run()
+    # To initialize train_parameter.
+    test_app = QApplication(sys.argv)
+    settings = QSettings('test_compiler', 'test_compiler')
+
+    painter = Painter2D()
+    test_window = MainWindow(painter=painter)
+    train_configuration.TrainDialog(test_window, settings=settings)
+
+    graph = Graph(painter=painter)
+    graph.load_from_json('../../examples/mnist.ppy')
+    graph.execute()
+
+    assert filecmp.cmp('ExampleNet.py', 'expect.txt')
