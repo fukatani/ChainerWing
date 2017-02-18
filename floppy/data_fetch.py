dcd27e5a2210f7bc823d665a66d5454baf6e983c
fukatani <nannyakannya@gmail.com>
2017-02-18 21:55:33 +0900

    Add datamanager test

--- floppy/data_fetch.py
+++ floppy/data_fetch.py
@@ -6,13 +6,17 @@ class DataManeger(object):
     def __init__(self):
         self.train_columns = 0
 
-    def get_data_from_file(file_name, train):
+    def get_data_from_file(self, file_name, is_supervised):
         if file_name[-4:] == '.csv':
-            return self.csv_to_ndarray(file_name, train)
+            return self.csv_to_ndarray(file_name, is_supervised)
         elif file_name[-4:] == '.npz':
-            np.
+            data = np.load(file_name)
+            if is_supervised:
+                return data['x'], data['y']
+            else:
+                return data['x'], None
 
-    def csv_to_ndarray(self, csv_file, train):
+    def csv_to_ndarray(self, csv_file, is_supervised):
         exists_header = 0
         with open(csv_file, 'r') as f:
             reader = csv.reader(f)
@@ -20,21 +24,22 @@ class DataManeger(object):
                 if isinstance(line[0], str):
                     exists_header = 1
         data = np.loadtxt(csv_file, delimiter=',', skiprows=exists_header)
-        return self.separate_superviser(train, data)
+        return self.separate_superviser(data, is_supervised)
 
-    def separate_superviser(self, train, data):
-        if train:
+    def separate_superviser(self, data, is_supervised):
+        if is_supervised:
             self.train_columns = data.shape[1]
             superviser = data[:, -1]
-            data = data[:, -1]
+            data = data[:, :-1]
             return data, superviser
         elif data.shape[1] == self.train_columns:
             superviser = data[:, -1]
-            data = data[:, -1]
+            data = data[:, :-1]
             return data, superviser
         else:  # Not including superviser
             return data, None
 
 
 if __name__ == '__main__':
-    train = DataManeger().csv_to_ndarray('sample_data.csv', True)
+    train_x, train_y = DataManeger().get_data_from_file('sample_data.csv', True)
+    train_x, train_y = DataManeger().get_data_from_file('sample_data.npz', True)
---
+++ floppy/test/test_data_fetch.py
@@ -0,0 +1,21 @@
+from floppy.data_fetch import DataManeger
+import numpy as np
+
+
+if __name__ == '__main__':
+    expect_x = np.array([[3., 4.],
+                         [3., 5.],
+                         [2., 6.],
+                         [2., 7.],
+                         [1., 8.],
+                         [1., 9.]])
+    expect_y = np.array([ 0.,  0.,  0.,  0.,  1.,  1.])
+
+    train_x, train_y = DataManeger().get_data_from_file('sample_data.csv', True)
+    assert (train_x == expect_x).all()
+    assert (train_y == expect_y).all()
+
+    np.savez('sample_data.npz', x=train_x, y=train_y)
+    train_x, train_y = DataManeger().get_data_from_file('sample_data.npz', True)
+    assert (train_x == expect_x).all()
+    assert (train_y == expect_y).all()
