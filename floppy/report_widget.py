011819e48db8fe35d45776d6aa2219b9231a8a48
fukatani <nannyakannya@gmail.com>
2017-02-20 22:22:03 +0900

    fix report widget

--- floppy/report_widget.py
+++ floppy/report_widget.py
@@ -1,68 +1,30 @@
-from PyQt5 import QtCore, QtGui, QtWidgets#, QtWebKitWidgets, QtWebKit
-try:
-    from PyQt5 import QtWebEngineWidgets, QtWebEngineCore
-except ImportError:
-    from PyQt5 import QtWebKitWidgets, QtWebKit
-    WebView = QtWebKitWidgets.QWebView
-else:
-    WebView = QtWebEngineWidgets.QWebEngineView
-from PyQt5.QtCore import Qt, QPoint, QSettings
-from floppy.templates import TEMPLATES
+from PyQt5.QtWidgets import QFrame
+from PyQt5.QtWidgets import QHBoxLayout
+from PyQt5.QtWidgets import QLabel
+from PyQt5.QtWidgets import QWidget
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtGui import QPainter
+from PyQt5.QtCore import Qt
+from PyQt5.QtCore import QPoint
 
-class ReportWidget(WebView):
+
+class ReportWidget(QWidget):
 
     def __init__(self, *args, **kwargs):
-        super(ReportWidget, self).__init__(*args, **kwargs)
+        super(ReportWidget, self).__init__(height=200, *args, **kwargs)
         self.setStyleSheet('''ReportWidget{background: rgb(55,55,55)}
         ''')
-        self.data = None
-        self.cache = []
-        self.templateCache = {}
-        self.setHtml('')
-        #import floppy.templates
+        self.pixmap = QPixmap("result/loss.png")
+        self.resize(200, 200)
 
-    def updateReport(self, data):
-        if data == self.data:
-            return
-        settings = QSettings('Floppy', 'Floppy')
-        self.fileBase = settings.value('WorkDir', type=str)
-        self.data = data
-        self._update()
-
-    def _update(self):
-        data = self.data
-        if not data:
-            return
-        try:
-            keep = data['keep']
-        except KeyError:
-            pass
-        else:
-            if keep:
-                if keep == 'CLEAR':
-                    self.cache = []
-                elif data[keep]:
-                    self.cache += data[keep]
-                    # print('xxxxx', self.cache)
-
-        try:
-            tmplt = self.templateCache[data['ID']]
-        except KeyError:
-            try:
-                tmplt = TEMPLATES[data['template']]()
-            except KeyError:
-                print('Error: {} template missing'.format(data['template']))
-                return
-            else:
-                self.templateCache[data['ID']] = tmplt
-
-        #tmplt = defaultTemplate
-        # url = QtCore.QUrl.fromLocalFile(self.fileBase)
-        url = QtCore.QUrl.fromLocalFile(QtCore.QDir(self.fileBase).absoluteFilePath('dummy.html'))
-        # QtWebKit.QWebSettings.clearMemoryCaches()
-        try:
-            QtWebEngineCore.QWebSettings.clearMemoryCaches()
-        except:
-            pass
-        #scrollValue = self.page().scrollPosition()
-        self.setHtml(tmplt(data, self.cache[:], self.fileBase, self.width()), url)
+    def paintEvent(self, event):
+        self.adjustSize()
+        size = self.size()
+        painter = QPainter(self)
+        point = QPoint(0, 0)
+        scaledPix = self.pixmap.scaled(size, Qt.KeepAspectRatio,
+                                       transformMode=Qt.SmoothTransformation)
+        # start painting the label from left upper corner
+        point.setX((size.width() - scaledPix.width()) / 2)
+        point.setY((size.height() - scaledPix.height()) / 2)
+        painter.drawPixmap(point, scaledPix)
