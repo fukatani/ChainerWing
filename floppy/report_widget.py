1e7a6b0fbbdb57053d3510b67c95c5d7e2fb6b81
fukatani <nannyakannya@gmail.com>
2017-02-22 22:20:10 +0900

    Enable to display accuracy graph

--- floppy/report_widget.py
+++ floppy/report_widget.py
@@ -1,27 +1,47 @@
 from floppy.train_configuration import TrainParamServer
 
 from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QTabWidget
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtGui import QPainter
 from PyQt5.QtCore import Qt
 from PyQt5.QtCore import QPoint
 
 
-class ReportWidget(QWidget):
+class ReportWidget(QTabWidget):
 
     def __init__(self, *args, **kwargs):
-        super(ReportWidget, self).__init__(height=200, *args, **kwargs)
+        super(ReportWidget, self).__init__(height=210, *args, **kwargs)
         self.setStyleSheet('''ReportWidget{background: rgb(55,55,55)}
         ''')
         try:
             loss_image = TrainParamServer()['WorkDir'] + "result/loss.png"
         except KeyError:
             loss_image = "result/loss.png"
-        self.pixmap = QPixmap(loss_image)
+        self.addTab(GraphWidget(loss_image, parent=self), 'Loss')
+        try:
+            acc_image = TrainParamServer()['WorkDir'] + "result/accuracy.png"
+        except KeyError:
+            acc_image = "result/accuracy.png"
+        self.addTab(GraphWidget(acc_image, parent=self), 'Accuracy')
         self.resize(200, 200)
 
+
+class GraphWidget(QWidget):
+
+    def __init__(self, image_file, *args, **kwargs):
+        super(GraphWidget, self).__init__(height=200)
+        self.setStyleSheet('''ReportWidget{background: rgb(55,55,55)}
+        ''')
+        self.pixmap = None
+        self.image_file = image_file
+
     def paintEvent(self, event):
-        self.adjustSize()
+        if 'Class' not in TrainParamServer()['TrainMode']:
+            if 'accuracy' in self.image_file:
+                return
+        self.pixmap = QPixmap(self.image_file)
+        #self.adjustSize()
         size = self.size()
         painter = QPainter(self)
         point = QPoint(0, 0)
