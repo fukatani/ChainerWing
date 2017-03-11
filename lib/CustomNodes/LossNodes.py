09187eedc42a8c71b0448f11272ec3acb973fd5c
fukatani <nannyakannya@gmail.com>
2017-03-11 14:30:13 +0900

    clean up

--- lib/CustomNodes/LossNodes.py
+++ lib/CustomNodes/LossNodes.py
@@ -5,7 +5,6 @@ from lib.node import Input, Loss
 
 class SoftmaxCrossEntropy(Loss):
     Input('in_array', chainer.Variable)
-    Input('ground_truth', chainer.Variable)
 
     def call(self):
         return 'softmax_cross_entropy(self.y, t)'
