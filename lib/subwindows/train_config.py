a50bd6bbe063cb06c90ec49b8888b4a400f233e8
fukatani <nannyakannya@gmail.com>
2017-03-17 23:26:52 +0900

    slight refactoring

--- lib/subwindows/train_config.py
+++ lib/subwindows/train_config.py
@@ -26,7 +26,7 @@ class TrainParamServer(object):
 
     def iter_for_opt_params(cls):
         for param in cls.__dict__:
-            if param[:4] == 'opt_':
+            if param.beginswith('opt_'):
                 yield param
 
     def clear_opt_params(cls):
