cbdd37e7245dbc4f00fb36a4f41ce78ea9965448
fukatani <nannyakannya@gmail.com>
2017-04-01 17:35:00 +0900

    Add activation

--- lib/CustomNodes/ActivationNodes.py
+++ lib/CustomNodes/ActivationNodes.py
@@ -51,3 +51,43 @@ class Dropout(Function):
     @classmethod
     def register_chainer_impl(cls):
         return chainer.functions.dropout
+
+
+class Elu(Function):
+    Input('in_array', chainer.Variable)
+    Input('alpha', float)
+    Output('out_array', chainer.Variable)
+
+    def call(self):
+        self.check_member(('_alpha',))
+        return self.ID + ' = elu(alpha={0}, x='.format(self._elu)
+
+    @classmethod
+    def register_chainer_impl(cls):
+        return chainer.functions.elu
+
+
+class ClippedRelu(Function):
+    Input('in_array', chainer.Variable)
+    Input('z', float)
+    Output('out_array', chainer.Variable)
+
+    def call(self):
+        self.check_member(('_alpha',))
+        return self.ID + ' = clipped_relu(z={0}, x='.format(self._elu)
+
+    @classmethod
+    def register_chainer_impl(cls):
+        return chainer.functions.clipped_relu
+
+
+class HardSigmoid(Function):
+    Input('in_array', chainer.Variable)
+    Output('out_array', chainer.Variable)
+
+    def call(self):
+        return self.ID + ' = hard_sigmoid('
+
+    @classmethod
+    def register_chainer_impl(cls):
+        return chainer.functions.hard_sigmoid
